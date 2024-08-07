6ff2df6f4d38bd891d6074ab3f8d1211cfe8b258
at3001 <70270768+at3001@users.noreply.github.com>
2024-08-07 02:27:59 -0400

    Update IBBusinessChatbot.py

--- IBBusinessChatbot.py
+++ IBBusinessChatbot.py
@@ -5,100 +5,76 @@ from langchain.prompts import PromptTemplate
 import os
 
 from langchain_chroma import Chroma
-
-from langchain_openai import OpenAIEmbeddings
-
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 import nest_asyncio
-
 from llama_parse import LlamaParse
 
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_chroma import Chroma
 from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
 from langchain_core.documents import Document
 
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 
 
 
-st.set_page_config(page_title="Document Genie", layout="wide")
+from openai import OpenAI
+api_key = 'sk-proj-gJQdBTPV8gFbZSjmcp2ET3BlbkFJwqNA0XMsVd7wpfCAlxO5'
 
-st.markdown("""
-## Document Genie: Get instant insights from your Documents
+st.session_state.client = OpenAI(api_key = api_key)
 
-This chatbot is built using the Retrieval-Augmented Generation (RAG) framework, leveraging Google's Generative AI model Gemini-PRO. It processes uploaded PDF documents by breaking them down into manageable chunks, creates a searchable vector store, and generates accurate answers to user queries. This advanced approach ensures high-quality, contextually relevant responses for an efficient and effective user experience.
-
-### How It Works
-
-Follow these simple steps to interact with the chatbot:
-
-1. **Enter Your API Key**: You'll need a Google API key for the chatbot to access Google's Generative AI models. Obtain your API key https://makersuite.google.com/app/apikey.
-
-2. **Upload Your Documents**: The system accepts multiple PDF files at once, analyzing the content to provide comprehensive insights.
-
-3. **Ask a Question**: After processing the documents, ask any question related to the content of your uploaded documents for a precise answer.
-""")
-
-
-
-# This is the first API key input; no need to repeat it in the main function.
-name = st.text_input("Enter your name", type="password", key="name")
 
 # Initialize session state variables
-if 'vectorstore_created' not in st.session_state:
+if 'chat_history' not in st.session_state:
+    st.session_state.chat_history = []
+
+if 'vectorstore' not in st.session_state:
+    print("dwosifhowqihgoiq2hgowiehgowiehgoiwheogiwhegoiwheogweogiwhgeoiwheogiwhg")
     st.session_state.vectorstore = None
 
-if 'history_aware_retriever_created' not in st.session_state:
+if 'history_aware_retriever' not in st.session_state:
     st.session_state.history_aware_retriever = None
-
 
 if 'conversational_rag_chain' not in st.session_state:
     st.session_state.conversational_rag_chain = None
 
+if 'llm' not in st.session_state:
+    st.session_state.llm = None
+
+if 'contextualize_q_chain' not in st.session_state:
+    st.session_state.contextualize_q_chain = None
+
+if 'valid_q_chain' not in st.session_state:
+    st.session_state.valid_q_chain = None
+
 def get_pdf_text():
-
     nest_asyncio.apply()
-
     parser = LlamaParse(
-        api_key="llx-OmxZDgHY0WOPAYcxtKPD33KPQ56KPys4MGeYyVIlca2tqs4v",  # can also be set in your env as LLAMA_CLOUD_API_KEY
-        result_type="markdown",  # "markdown" and "text" are available
-        num_workers=4,  # if multiple files passed, split in `num_workers` API calls
+        api_key="llx-OmxZDgHY0WOPAYcxtKPD33KPQ56KPys4MGeYyVIlca2tqs4v",
+        result_type="markdown",
+        num_workers=4,
         verbose=True,
-        language="en",  # Optionally you can define a language, default=en
+        language="en",
     )
-
-# sync
     documents = parser.load_data("/Users/akshat/Downloads/ocr_unit1.pdf")
-
     text = '\n\n'.join([d.text for d in documents])
-
     return text
 
 def create_vector_store(text):
-
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     splits = text_splitter.split_text(text)
     documents = [Document(page_content=split) for split in splits]
+    vectorstore = Chroma.from_documents(documents=documents, embedding=OpenAIEmbeddings(openai_api_key="sk-proj-gJQdBTPV8gFbZSjmcp2ET3BlbkFJwqNA0XMsVd7wpfCAlxO5"))
+    st.session_state.vectorstore = vectorstore.as_retriever()
 
-    vectorstore = Chroma.from_documents(documents=documents, embedding=OpenAIEmbeddings(openai_api_key = "sk-proj-gJQdBTPV8gFbZSjmcp2ET3BlbkFJwqNA0XMsVd7wpfCAlxO5"))
-
-    retriever = vectorstore.as_retriever()
-
-    st.session_state.vectorstore = retriever
-
-
-def create_retriever_with_history(retriever = st.session_state.vectorstore):
-    llm = ChatOpenAI(model="gpt-4o", openai_api_key = "sk-proj-gJQdBTPV8gFbZSjmcp2ET3BlbkFJwqNA0XMsVd7wpfCAlxO5", temperature=0)
-
-    # retriever = Chroma.load_local("chroma_retriever", OpenAIEmbeddings(api_key = "sk-proj-gJQdBTPV8gFbZSjmcp2ET3BlbkFJwqNA0XMsVd7wpfCAlxO5"))
-    ### Contextualize question ###
+def create_retriever_with_history():
+    st.session_state.llm = ChatOpenAI(model="gpt-4", openai_api_key="sk-proj-gJQdBTPV8gFbZSjmcp2ET3BlbkFJwqNA0XMsVd7wpfCAlxO5", temperature=0)
+    
     contextualize_q_system_prompt = (
         "Given a chat history and the latest user question "
         "which might reference context in the chat history, "
@@ -106,6 +82,8 @@ def create_retriever_with_history(retriever = st.session_state.vectorstore):
         "without the chat history. Do NOT answer the question, "
         "just reformulate it if needed and otherwise return it as is."
     )
+
+
     contextualize_q_prompt = ChatPromptTemplate.from_messages(
         [
             ("system", contextualize_q_system_prompt),
@@ -113,27 +91,25 @@ def create_retriever_with_history(retriever = st.session_state.vectorstore):
             ("human", "{input}"),
         ]
     )
-    history_aware_retriever = create_history_aware_retriever(
-        llm, retriever, contextualize_q_prompt
+
+    st.session_state.contextualize_q_chain = (contextualize_q_prompt | st.session_state.llm).with_config(
+        tags=["contextualize_q_chain"]
     )
 
-    st.session_state.history_aware_retriever = history_aware_retriever
-    st.session_state.llm = llm
+    st.session_state.history_aware_retriever = create_history_aware_retriever(
+        st.session_state.llm, st.session_state.vectorstore, contextualize_q_prompt
+    )
+    # st.session_state.history_aware_retriever.include_metadata = True  # Include metadata
 
-    # return history_aware_retriever
-
-
-def get_conversational_chain(history_aware_retriever, llm):
-
+def get_conversational_chain():
     system_prompt = (
-    "You are an assistant for question-answering tasks related to IB Business. "
-    "You function as an automated tutor and your goal is to help students better understand content. "
-    "Use the following pieces of retrieved context to answer "
-    "the question. If you don't know the answer, say that you "
-    "don't know. "
-    "\n\n"
-    "{context}"
-)
+        "You are an assistant for question-answering tasks related to IB Business. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. "
+        "\n\n"
+        "{context}"
+    )
 
     qa_prompt = ChatPromptTemplate.from_messages(
         [
@@ -142,20 +118,17 @@ def get_conversational_chain(history_aware_retriever, llm):
             ("human", "{input}"),
         ]
     )
-    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
-
-    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+    question_answer_chain = create_stuff_documents_chain(st.session_state.llm, qa_prompt)
+    rag_chain = create_retrieval_chain(st.session_state.history_aware_retriever, question_answer_chain)
 
     store = {}
-
 
     def get_session_history(session_id: str) -> BaseChatMessageHistory:
         if session_id not in store:
             store[session_id] = ChatMessageHistory()
         return store[session_id]
 
-
-    conversational_rag_chain = RunnableWithMessageHistory(
+    st.session_state.conversational_rag_chain = RunnableWithMessageHistory(
         rag_chain,
         get_session_history,
         input_messages_key="input",
@@ -163,85 +136,119 @@ def get_conversational_chain(history_aware_retriever, llm):
         output_messages_key="answer",
     )
 
-    st.session_state.conversational_rag_chain = conversational_rag_chain
 
+def validate_q_doc(question, docs):
 
+    #Need to error proof this prompt using some kind of parser or validator
+    prompt = """
+    You are given a 'question' and a set of 'documents' that the question is support to be answered from.
+    You are supposed to figure out whether the question can be answered based on the given documents.
 
+    -----------
+    Question: {question}
 
+    -----------
+    Answer:
+    {documents}
+    -----------
 
+    If the question can be answered using information from the documents, respond with a 'yes', otherwise respond with a 'no'.
+    Do not respond with anything but a 'yes' or 'no'
+    """.format(question = question, documents = '\n'.join([d for d in docs]))
 
+        
+    response = st.session_state.client.completions.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt = prompt,
+        max_tokens = 50,
+        temperature = 0
+    )
 
+    response = response.choices[0].text.strip().lower()
 
+    print(f"response: {response}")
 
+    if response == 'yes':
+        return True
+    else:
+        return False
 
-    # prompt_template = """
-    # Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    # provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
-    # Context:\n {context}?\n
-    # Question: \n{question}\n
-
-    # Answer:
-    # """
-    # model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3, google_api_key=api_key)
-    # prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
-    # chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
-    # return chain
-
-
-def validate_document_viability():
     # We use this to validate if the given documents are appropriate to answer the question or not
+
+def check_security_input():
+    #Using ShieldGemma to validate input
     pass
 
-def user_input(user_question, conversational_rag_chain):
-    response = conversational_rag_chain.invoke(
-                    {"input": user_question},
-                    config={
-                        "configurable": {"session_id": "abc123"}
-                    },  # constructs a key "abc123" in `store`.
-                )["answer"]
+def check_security_output():
+    #Using ShieldGemma to validate output
+    pass
 
-    
-
-
-    # embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
-    # new_db = FAISS.load_local("faiss_index", embeddings)
-    # docs = new_db.similarity_search(user_question)
-    # chain = get_conversational_chain()
-    # response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
-    st.write("Reply: ", response)
 
 def main():
-    st.header("Diplomaly Q&A Chatbot")
+    st.set_page_config(page_title="Document Genie", layout="wide")
+    st.header("Document Genie Q&A Chatbot")
 
+    # Initialize all components if they haven't been created yet
     if st.session_state.vectorstore is None:
+        with st.spinner("Initializing the chatbot..."):
+            text = get_pdf_text()
+            create_vector_store(text)
+            create_retriever_with_history()
+            get_conversational_chain()
+        st.success("Chatbot initialized successfully!")
 
-        #for now we dont pass in anything here;
+    # Main chat interface
+    st.subheader("Chat with Document Genie")
 
-        text = get_pdf_text()
-        create_vector_store(text)
-        print("done with this shit")
+    # Display chat history
+    chat_container = st.container()
+    with chat_container:
+        for message in st.session_state.chat_history:
+            with st.chat_message(message["role"]):
+                st.write(message["content"])
 
-    if st.session_state.history_aware_retriever is None:
-        create_retriever_with_history(st.session_state.vectorstore)
+    # User input
+    user_question = st.chat_input("Ask a question about the documents")
 
-    if st.session_state.conversational_rag_chain is None:
-        get_conversational_chain(st.session_state.history_aware_retriever, st.session_state.llm)
+    #Reframed question
+    reframed_question = st.session_state.contextualize_q_chain.invoke({"input": user_question, "chat_history": [m["content"] for m in st.session_state.chat_history]})
+    # print("reframed question " + str(reframed_question))
+
+    if user_question:
+        # Add user message to chat history
+        st.session_state.chat_history.append({"role": "user", "content": user_question})
+
+        # Generate response
+        with st.spinner("Thinking..."):
+            response = st.session_state.conversational_rag_chain.invoke(
+                {"input": user_question},
+                config={"configurable": {"session_id": "abc123"}}
+            )
+            answer = response["answer"]
+
+            retrieved_docs = [d.page_content for d in response.get("context", [])]
+
+            print(f"retrieved docs {retrieved_docs}")
 
 
-    user_question = st.text_input("Ask any question about Unit 1", key="user_question")
+            #Check validity of the responses
 
-    if user_question:  # Ensure API key and user question are provided
-        user_input(user_question, st.session_state.conversational_rag_chain)
+            q_valid =  validate_q_doc(user_question, retrieved_docs)
 
-    # with st.sidebar:
-    #     st.title("Menu:")
-    #     pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True, key="pdf_uploader")
-    #     if st.button("Submit & Process", key="process_button") and api_key:  # Check if API key is provided before processing
-    #         with st.spinner("Processing..."):
-    #             raw_text = get_pdf_text(pdf_docs)
-    #             text_chunks = get_text_chunks(raw_text)
-    #             get_vector_store(text_chunks, api_key)
-    #             st.success("Done")
+            if not q_valid:
+                answer = "Sorry, your question could not be answered based on our source material."
+
+
+
+            
+
+            
+
+        # Add assistant response to chat history
+        st.session_state.chat_history.append({"role": "assistant", "content": answer})
+
+        # Rerun to update the chat display
+        st.rerun()
 
 if __name__ == "__main__":
     main()
